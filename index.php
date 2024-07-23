1aeb09cada267ec4531019b5c2aa6c95029f2e2a
Christian Vincent Villanueva <villanuevachristianvincent@gmail.com>
2024-07-23 21:04:07 +0800

    pull up buds

--- index.php
+++ index.php
@@ -7,21 +7,21 @@ require 'Login/controllers/emailver.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Hospital | Appointment System</title>
+    <title>ReNewIt</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
     <header class="header">
-        <a href="#" class="logo"> <i class="fas fa-stethoscope"></i> <strong>Med</strong>Appoint</a>
+        <a href="#" class="logo"> <i class="fas fa-phone-square-alt"></i> <strong>Refurbished</strong>Gadget</a>
 
         <nav class="navbar">
             <a href="#home">home</a>
             <a href="#about">about</a>
             <a href="#services">services</a>
-            <a href="#doctors">doctors</a>
-            <a href="#appointment">appointment</a>
+            <a href="#doctors">products</a>
+            <a href="#appointment">contact us</a>
             <?php if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] === false): ?>
             <!-- Show "Sign in/Sign up" button when not logged in -->
             <a href="Login/login.php">Sign in/Sign up</a>
